44045926edb66cbfbc29cb7f7af9a48cbb981312
himakarbavikaty <himakar.bavikaty@gmail.com>
2024-07-27 06:49:03 +0530

    updated code

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from src.llm import ask_bot
 
 
 @cl.on_message
-async def main(message: cl.Message):
+async def main(user_message: cl.Message):
+
+    response=ask_bot(user_message.content)
     # Your custom logic goes here...
 
     # Send a response back to the user
     await cl.Message(
-        content=f"Received: {message.content}",
+        content=f"Received: {response.content}",
     ).send()
--- src/llm.py
+++ src/llm.py
@@ -12,5 +12,10 @@ def ask_bot(user_message):
     messages = [{"role":"system", "content":instruction},{"role":"user", "content":user_message}]
     llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest")
     responses = llm.invoke(messages)
-    return responses.content
+    return responses
+
+if __name__ == "__main__":
+    user_message = "hi how are you?"
+    responses = ask_bot(user_message)
+    print(responses.content)
 
